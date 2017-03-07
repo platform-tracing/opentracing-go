3df112ee9b276b4a02e9bc6c8bb37834bab143dc
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2017-03-07 10:56:55 +0300

    Add observer API

---
+++ observer.go
@@ -0,0 +1,14 @@
+package opentracing
+
+
+// Observer can be registered with the Tracer to receive notifications about new Spans.
+type Observer interface {
+    OnStartSpan(sp Span, operationName string, options StartSpanOptions) SpanObserver
+}
+
+// SpanObserver is created by the Observer and receives notifications about other Span events.
+type SpanObserver interface {
+    OnSetOperationName(operationName string)
+    OnSetTag(key string, value interface{})
+    OnFinish(options FinishOptions)
+}
